16d22f3f1dcafa0f01e1b5f28d90bbb83be8d6b1
AdonaiDiazEsparza <carlosadonai.de0208@gmail.com>
2024-09-19 13:40:51 -0600

    feat: icon added

--- App_created/app_user.c
+++ App_created/app_user.c
@@ -3,6 +3,11 @@
 #include <gui/gui.h>
 #include <gui/view_dispatcher.h>
 
+// This library is included for the icons
+// The name will be named by the app id and
+// Adding "_icons"
+#include "simple_app_view_disp_icons.h"
+
 // Create my struct to controlate my GUIs
 typedef struct {
     ViewDispatcher* view_dispatcher;
@@ -18,8 +23,15 @@ typedef enum {
 static void my_draw_callback(Canvas* canvas, void* context) {
     furi_assert(context);
     UNUSED(context);
+
+    // Draw an icon
+    canvas_draw_icon(canvas, 60, 20, &I_cat);
+
+    // Set the font of the text
     canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str_aligned(canvas, 30, 60, AlignCenter, AlignCenter, "HelloWorld");
+
+    // draw a string
+    canvas_draw_str_aligned(canvas, 60, 50, AlignCenter, AlignCenter, "HelloWorld!");
 }
 
 // This function works for the inpust on the view
@@ -68,11 +80,49 @@ App* app_alloc() {
     // Set the orientation of the view
     view_set_orientation(app->my_view, ViewOrientationHorizontal);
 
+    // We set the context in the view dispatcher
+    view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
+
+    // Set the function of custom event
+    view_dispatcher_set_custom_event_callback(
+        app->view_dispatcher, view_dispatcher_custom_event_callback);
+
+    // Set the navegation event callback
+    view_dispatcher_set_navigation_event_callback(
+        app->view_dispatcher, view_dispatcher_navegation_event_callback);
+
+    // Add the view to a view dispatcher
+    view_dispatcher_add_view(app->view_dispatcher, FirstView, app->my_view);
+
     return app;
+}
+
+void app_free(App* app) {
+    // Remove the views from the view dispatcher
+    view_dispatcher_remove_view(app->view_dispatcher, FirstView);
+
+    // Free memory
+    view_free(app->my_view);
+
+    // Free viewdispatcher
+    view_dispatcher_free(app->view_dispatcher);
+
+    // Free struct
+    free(app);
 }
 
 int32_t app_main(void* p) {
     UNUSED(p);
+
+    App* app = app_alloc();
+    Gui* gui = furi_record_open(RECORD_GUI);
+
+    view_dispatcher_attach_to_gui(app->view_dispatcher, gui, ViewDispatcherTypeFullscreen);
+    view_dispatcher_switch_to_view(app->view_dispatcher, FirstView);
+
+    view_dispatcher_run(app->view_dispatcher);
+
+    app_free(app);
 
     return 0;
 }
